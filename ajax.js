14ca08877ffa27a13eeb56a0f50de997fa168874
KaiserChan <chankaiserkk@gmail.com>
2017-10-03 14:19:57 -0400

    updated first-ajax ... completed step 1 and step 2

--- ajax.js
+++ ajax.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   /* Your code goes here */
+  var button = document.querySelector('button');
+
+  button.addEventListener('click', clickButton);
+
+  function clickButton() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com',
+      method: 'GET',
+      data: {},
+      dataType: 'text'
+    });    
+  }
+
+
+
 
 });
